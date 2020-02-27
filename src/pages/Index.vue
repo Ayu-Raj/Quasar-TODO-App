0fe7b1315d081d489666e2c3fbd24057c8255552
ayush <ayushrajmathur@gmail.com>
2020-02-27 17:16:30 +0530

    Initial Commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-page class="flex flex-center">
+    <ul>
+
+
+      <task 
+      v-for="(task, index) in tasks"
+      :key="task.id"
+      :task="task"
+      :index="index"></task>
+    </ul>
+  </q-page>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          name: 'Go to shop',
+          dueDate: '2019/02/12',
+          dueTime: '18:30'
+        },
+        {
+          id: 2,
+          name: 'Get Bananas',
+          dueDate: '2020/02/22',
+          dueTime: '12:30'
+        },
+        {
+          id: 3,
+          name: 'Get Apples',
+          dueDate: '2020/01/11',
+          dueTime: '16:30'
+        }
+      ]
+    }
+    },
+    methods: {
+      deleteTask(index){
+        this.tasks.splice(index,1)
+      }
+      
+  },
+  components: {
+        'task': require('components/Task.vue').default
+      }
+}
+</script>
---
+++ src/pages/PageTodo.vue
@@ -0,0 +1,65 @@
+<template>
+  <q-page class="q-pa-md">
+    <q-list bordered >
+
+      <q-item  
+	  v-for="task in tasks"
+	  :key="task.id"
+	  @click="task.completed=!task.completed"
+	  clickable
+	  v-ripple>
+        <q-item-section side top>
+          <q-checkbox v-model="task.completed" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ task.name }}</q-item-label>
+        </q-item-section>
+		<q-icon 
+		name="event"
+		size="18px"
+		/>
+		 <q-item-section side top>
+          <q-item-label caption>{{ task.dueDate }}</q-item-label>
+		   <q-item-label caption><small>{{ task.dueTime }}</small></q-item-label>
+        </q-item-section>
+      </q-item>
+ </q-list>
+  </q-page>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				tasks: [
+        {
+          id: 1,
+          name: 'Go to shop',
+          dueDate: '2019/02/12',
+		  dueTime: '18:30',
+		  completed: false
+        },
+        {
+          id: 2,
+          name: 'Get Bananas',
+          dueDate: '2020/02/22',
+          dueTime: '12:30',
+		  completed: false
+        },
+        {
+          id: 3,
+          name: 'Get Apples',
+          dueDate: '2020/01/11',
+          dueTime: '16:30',
+		  completed: false
+        }
+      ]
+			}
+		}
+	}
+</script>
+
+<style>
+	
+</style>
